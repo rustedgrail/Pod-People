f67155869eaefc96ce3a9dd0202beea543cfc536
David Newell <beta@alumni.rice.edu>
2011-03-07 11:27:17 -0500

    rails basics

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PodPeople::Application.config.secret_token = '7f6fbbf53616ec7b6a190a0d7fdce6db837b6d72d41a58cf8c2b9221c1a689b9daa0b46e416d3de765a44b010e8c9561eb74b7ee60131b7954d04dd546a5b7b5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+PodPeople::Application.config.session_store :cookie_store, :key => '_pod-people_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# PodPeople::Application.config.session_store :active_record_store
